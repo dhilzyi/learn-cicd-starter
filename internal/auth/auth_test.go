1823a003dc3cfa348543ba54d8492752ce808f61
dhilzyi <revaldiefahrezal@gmail.com>
2026-04-01 23:32:54 +0700

    add test for auth module, update to run unit test and remove go version test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,64 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAuth(t *testing.T) {
+	tests := []struct {
+		name     string
+		key      string
+		err      error
+		expected string
+	}{
+		{
+			name:     "check key",
+			key:      "ApiKey 12345",
+			err:      nil,
+			expected: "12345",
+		},
+		{
+			name:     "normal key",
+			key:      "12341fkhjalfy29fy10fh12",
+			err:      nil,
+			expected: "12341fkhjalfy29fy10fh12",
+		},
+
+		{
+			name:     "header not include",
+			key:      "",
+			err:      ErrNoAuthHeaderIncluded,
+			expected: ErrNoAuthHeaderIncluded.Error(),
+		},
+		{
+			name:     "malformed api key",
+			key:      "12345",
+			err:      errors.New("malformed authorization header"),
+			expected: "malformed authorization header",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := http.Header{}
+			h.Add("Authorization", tt.key)
+
+			result, err := GetAPIKey(h)
+			if err != nil {
+				if tt.err == nil {
+					t.Errorf("unexpected error: %v", err)
+					return
+				}
+				if err.Error() != tt.err.Error() {
+					t.Errorf("expected error '%s' got '%s'", tt.err.Error(), err.Error())
+				}
+				return
+			}
+			if result != tt.expected {
+				t.Errorf("expected %s got %s", tt.expected, result)
+			}
+		})
+	}
+}
